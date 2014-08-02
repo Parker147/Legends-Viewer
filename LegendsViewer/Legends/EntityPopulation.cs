a9ebe16100242372dba43c6cee5c6d40ae0892bd
Parker147 <thegreysloth@gmail.com>
2014-08-02 07:55:11 -0500

    Added race, count, entity to entity populations.

--- LegendsViewer/Legends/EntityPopulation.cs
+++ LegendsViewer/Legends/EntityPopulation.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace LegendsViewer.Legends
 {
     public class EntityPopulation : WorldObject
     {
+        public string Race { get; set; }
+        public int Count { get; set; }
+        public Entity Entity { get; set; }
+    
         public static List<string> Filters;
         public override List<WorldEvent> FilteredEvents
         {
@@ -15,7 +20,20 @@ namespace LegendsViewer.Legends
         public EntityPopulation(List<Property> properties, World world)
             : base(properties, world)
         {
-            
+            foreach (Property property in properties)
+            {
+                switch (property.Name)
+                {
+                    case "race":
+                        var raceCount = property.Value.Split(':');
+                        Race = raceCount[0];
+                        Count = Convert.ToInt32(raceCount[1]);
+                        break;
+                    case "civ_id":
+                        Entity = world.GetEntity(property.ValueAsInt());
+                        break;
+                }
+            }
         }
     }
 }
