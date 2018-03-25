eeec52dcddf0aec3167af0abe7ac71c05363079a
kromtec <thomas.svoboda@gmx.net>
2018-03-25 11:59:36 +0200

    updated merchants event

--- LegendsViewer/Legends/Events/Merchant.cs
+++ LegendsViewer/Legends/Events/Merchant.cs
@@ -7,6 +7,8 @@ namespace LegendsViewer.Legends.Events
     public class Merchant : WorldEvent
     {
         public Entity Source { get; set; }
+        public Entity TraderEntity { get; set; }
+        public Entity DepotEntity { get; set; }
         public Entity Destination { get; set; }
         public Site Site { get; set; }
 
@@ -17,9 +19,39 @@ namespace LegendsViewer.Legends.Events
             {
                 switch (property.Name)
                 {
-                    case "source": Source = world.GetEntity(Convert.ToInt32(property.Value)); break;
-                    case "destination": Destination = world.GetEntity(Convert.ToInt32(property.Value)); break;
-                    case "site": Site = world.GetSite(Convert.ToInt32(property.Value)); break;
+                    case "source":
+                    case "trader_entity_id":
+                        if (Source == null)
+                        {
+                            Source = world.GetEntity(Convert.ToInt32(property.Value));
+                        }
+                        else
+                        {
+                            property.Known = true;
+                        }
+                        break;
+                    case "destination":
+                    case "depot_entity_id":
+                        if (Destination == null)
+                        {
+                            Destination = world.GetEntity(Convert.ToInt32(property.Value));
+                        }
+                        else
+                        {
+                            property.Known = true;
+                        }
+                        break;
+                    case "site":
+                    case "site_id":
+                        if (Site == null)
+                        {
+                            Site = world.GetSite(Convert.ToInt32(property.Value));
+                        }
+                        else
+                        {
+                            property.Known = true;
+                        }
+                        break;
                 }
             }
             Source.AddEvent(this);
